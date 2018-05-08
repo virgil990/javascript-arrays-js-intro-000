95aa724f404bcee843cd109e84cabb3b5a09b589
Daniel Rubens <dan.rubens@gmail.com>
2018-05-08 03:37:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,5 +5,6 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  array.unshift(element);
+  return array;
 }
