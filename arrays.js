b276fa775bf5ea64d700b69c70b9020920eb4541
Daniel Rubens <dan.rubens@gmail.com>
2018-05-08 03:57:52 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,7 +28,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  return array.slice(0);
+  return array.slice(1);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
