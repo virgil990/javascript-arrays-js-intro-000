3650be436f8a71c7ad4508f6f81ed8c8c35e2fb0
Daniel Rubens <dan.rubens@gmail.com>
2018-05-08 03:00:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
 
-function addElementToBeginningOfArray(element, array) {
-  return array.unshift(element);
+function addElementToBeginningOfArray(array, element) {
+  return array === array.unshift(element);
 }
 
-function destructivelyAddElementToBeginningOfArray(element, array) {
+function destructivelyAddElementToBeginningOfArray(array, element) {
 
 }
